c8fd55fb9a93c50a1ed4281177885c6623eb5354
Lamieme <itsmelamieee1204@gmail.com>
2023-10-17 23:13:19 +0700

    submit

--- FinalProject/main.js
+++ FinalProject/main.js
@@ -1,7 +1,7 @@
 window.onload = () => {
-     const tab_switchers = document.querySelectorAll('[data-switcher]');
+    const tab_switchers = document.querySelectorAll('[data-switcher]');
 
-    for(let i = 0; i < tab_switchers.length; i++) {
+    for (let i = 0; i < tab_switchers.length; i++) {
         const tab_switcher = tab_switchers[i];
         const page_id = tab_switcher.dataset.tab;
 
@@ -10,26 +10,47 @@ window.onload = () => {
             tab_switcher.parentNode.classList.add('is-active');
 
             SwitchPage(page_id);
-            
+
         });
 
-        }
     }
+}
 
-    function SwitchPage(page_id) {
-        console.log(page_id);
-        const current_page = document.querySelector('.pages .page.is-active');
-        // if(current_page != null)
-        current_page.classList.remove('is-active');
-        
-        const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
-        next_page.classList.add('is-active');
+function SwitchPage(page_id) {
+    console.log(page_id);
+    const current_page = document.querySelector('.pages .page.is-active');
+    // if(current_page != null)
+    current_page.classList.remove('is-active');
+
+    const next_page = document.querySelector(`.pages .page[data-page="${page_id}"]`);
+    next_page.classList.add('is-active');
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function logInFunction() {
+
+}
+
+
+var btnOpen = document.querySelector('.register')
+var modal = document.querySelector('.modal-box')
+var btnClose = document.querySelector('.close')
+
+function toggleModal() {
+    modal.classList.toggle('hide')
+
+}
+btnOpen.addEventListener('click', toggleModal)
+btnClose.addEventListener('click', toggleModal)
+modal.addEventListener('click', function (e) {
+    if (e.target == e.currentTarget) {
+        toggleModal()
     }
-
-    function openForm() {
-        document.getElementById("myForm").style.display = "block";
-      }
-      
-      function closeForm() {
-        document.getElementById("myForm").style.display = "none";
-      }
+}) 
